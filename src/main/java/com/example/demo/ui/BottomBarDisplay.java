58b4f3a89afd27dd379a0ee57f383d351badd417
hichamahsaini3-cyber <hicham.ahsaini3@gmail.com>
2026-05-28 15:32:40 +0200

    Ajout d'une petit barre en bas de la fenêtre avec un bouton next Turn

---
+++ src/main/java/com/example/demo/ui/BottomBarDisplay.java
@@ -0,0 +1,44 @@
+package com.example.demo.ui;
+
+import com.example.demo.simulation.Simulation;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+
+public class BottomBarDisplay {
+
+    private Simulation simulation;
+    private Runnable refreshUI;
+
+    public BottomBarDisplay(Simulation simulation, Runnable refreshUI) {
+        this.simulation = simulation;
+        this.refreshUI = refreshUI;
+    }
+
+    public Parent createContent() {
+
+        HBox bottomBar = new HBox();
+        bottomBar.getStyleClass().add("bottom-bar");
+        bottomBar.setAlignment(Pos.CENTER_RIGHT);
+        bottomBar.setSpacing(15);
+        bottomBar.setPadding(new Insets(10, 20, 10, 20));
+
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+
+        Button nextTurn = new Button("=> Next Turn");
+        nextTurn.getStyleClass().add("nav-button");
+        nextTurn.setOnAction(e -> {
+            this.simulation.spreadFireOneTurn();
+            this.refreshUI.run();
+        });
+
+        bottomBar.getChildren().addAll(spacer, nextTurn);
+
+        return bottomBar;
+    }
+}
